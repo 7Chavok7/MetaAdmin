8c2e7bffc4828e5cdd9236223b4244b34dd0e195
Andrey Grachev <Chavoka@inbox.ru>
2026-06-24 09:44:21 +0300

    Добавлено meta_app/appendance/routing.py

--- meta_app/attendance/routing.py
+++ meta_app/attendance/routing.py
@@ -1,5 +1,7 @@
+# meta_app/attendance/routing.py
 from django.urls import re_path
 from .consumers import UpdateConsumer
+
 
 websocket_urlpatterns = [
     re_path(r'ws/updates/$', UpdateConsumer.as_asgi()),
