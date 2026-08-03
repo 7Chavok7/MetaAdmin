92df099fb14c143c6039f308984c7727a069827d
Grouter <Chavoka@inbox.ru>
2026-08-03 13:49:17 +0300

    Добавлена админка для отпусков

--- meta_app/attendance/admin.py
+++ meta_app/attendance/admin.py
@@ -1,4 +1,36 @@
+# /meta_app/
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .models import VacationRequest
 
 
+@admin.register(VacationRequest)
+class VacationRequestAdmin(admin.ModelAdmin):
+    """Админка запросов на отпуск"""
+
+    list_display = [
+        'employee',
+        'start_date',
+        'end_date',
+        'status',
+    ]
+    list_filter = [
+        'employee',
+        'status',
+    ]
+    search_fields = [
+        'employee',
+        'status',
+        'processed_by',   
+    ]
+    readonly_fields = [
+        'staus',
+        'processed_at',
+        'created_at',
+        'updated_at'
+    ]
+    fieldsets = (
+        ('Основные данные', {
+            'fields': ('employee', 'start_date', 'end_date', 'status')
+        }),
+    )
